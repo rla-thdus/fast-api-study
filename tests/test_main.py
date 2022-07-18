62905d71ab1fb8a8ced67fffc0c03c3422b1f408
rla-thdus <0228ksy@naver.com>
2022-07-18 22:00:10 +0900

    [Update] post data name change pwd to password

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,6 +7,6 @@ client = TestClient(app)
 
 
 def test_user_register_should_success_with_correct_data():
-    response = client.post("/register", json={"email": "ab", "pwd": "1234"})
+    response = client.post("/register", json={"email": "ab", "password": "1234"})
     assert response.status_code == 200
     assert response.json() == {"message": "register success"}
