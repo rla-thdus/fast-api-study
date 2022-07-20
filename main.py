c1a8c3b76fad6097c01f13c3b6f7d15800ae82ec
rlathdus <0228ksy@naver.com>
2022-07-20 20:54:18 +0900

    [Updata] api change /register to /user

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def get_users(db: Session = Depends(get_db)):
     return users
 
 
-@app.post("/register", response_model=schema.User)
+@app.post("/user", response_model=schema.User)
 def create_user(user: schema.UserBase, db: Session = Depends(get_db)):
     db_user = crud.check_duplicated_email(db, user.email)
     if db_user:
--- tests/test_main.py
+++ tests/test_main.py
@@ -7,13 +7,13 @@ client = TestClient(app)
 
 
 def test_user_register_should_success_with_correct_data():
-    response = client.post("/register", json={"email": "test2", "password": "1234"})
+    response = client.post("/user", json={"email": "test2", "password": "1234"})
     assert response.status_code == 200
     assert response.json() == {'email': 'test2', 'id': 4, 'is_active': True, 'password': '1234'}
 
 
 def test_user_register_should_fail_with_duplicated_email():
-    response = client.post("/register", json={"email": "ab", "password": "1234"})
+    response = client.post("/user", json={"email": "ab", "password": "1234"})
     assert response.status_code == 400
     assert response.json() == {'detail': 'email duplicated'}
 
